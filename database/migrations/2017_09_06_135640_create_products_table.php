262513373e174ea26767cf7cb8836f0f0491e9e1
Ayu5h <bhojraj.bhatta99@gmail.com>
2017-09-08 09:08:53 +0545

    products table updated

--- database/migrations/2017_09_06_135640_create_products_table.php
+++ database/migrations/2017_09_06_135640_create_products_table.php
@@ -13,15 +13,19 @@ class CreateProductsTable extends Migration
      */
     public function up()
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('online_examinations', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name', 100);
+            $table->string('title', 100);
             $table->string('slug', 100);
-            $table->text('description');
+            $table->string('subject_code', 100)->unique();
             $table->string('image', 100);
+            $table->text('description');
 
-            $table->bigInteger('product_category_id')->unsigned()->index();
-            $table->foreign('product_category_id')->references('id')->on('categories');
+            $table->unsignedInteger('expiry_months')->comment('In Months');
+            $table->date('expiry_date')->comment('Should be calculated from expiry_months');
+
+            $table->enum('renewable_fee_type', ['default', 'custom', 'non-renewable'])->index();
+            $table->double('renewable_fee', 15, 2);
 
             $table->bigInteger('sub_category_id')->unsigned()->index();
             $table->foreign('sub_category_id')->references('id')->on('sub_categories');
@@ -29,17 +33,23 @@ class CreateProductsTable extends Migration
             $table->bigInteger('grade_id')->unsigned()->index();
             $table->foreign('grade_id')->references('id')->on('grades');
 
-            $table->unsignedInteger('exam_duration')->comment('In Seconds');
+            $table->unsignedInteger('exam_duration')->comment('In Minutes');
+
+            $table->enum('marking_type', ['manual', 'automated-marking'])->index()->default('automated-marking'); // Computer Marked / Manually marked
+            $table->enum('is_manual_marking', ['yes', 'no']);
+
+            $table->string('certificate_type', 255)->index();
+            $table->enum('exam_type', ['online', 'offline'])->index();
+            $table->string('rising_software_key', 255);
+
             $table->double('default_price', 15, 2);
-            $table->unsignedInteger('expiry_months')->comment('In Months');
-            $table->enum('marking_type', ['computer', 'manual'])->index(); // Computer Marked / Manually marked
-            $table->enum('certificate_type', ['e-certificate', 'hard_copy'])->index(); // E-Certificate / Hard Copy
-            $table->enum('exam_type', ['online', 'offline'])->index(); // Online / Offline
-            $table->string('online_key', 100);
+
             $table->enum('pricing_policy', ['default', 'state'])->index(); // Default / State
-            $table->date('created_date');
-            $table->date('last_updated_date');
+
             $table->string('last_updated_by',200);
+            $table->bigInteger('last_updated_by_user')->unsigned()->index();
+            $table->foreign('last_updated_by_user')->references('id')->on('users');
+
             $table->enum('status', ['active', 'in_active'])->index();
             $table->timestamps();
         });
